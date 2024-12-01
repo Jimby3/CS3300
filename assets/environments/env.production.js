ae312e641c67154642b1995f9ab8a29b32c5395d
jimby3 <jimever333@gmail.com>
2024-12-01 10:59:56 -0700

    Auto-generated commit

--- assets/environments/env.production.js
+++ assets/environments/env.production.js
@@ -3,5 +3,5 @@
 
   // Public backend base URL for production
   // Still pointing to localhost since the backend is not public
-  window.__env.apiBaseUrl = 'https://09e0-2601-281-d780-66c0-00-b60b.ngrok-free.app';
+  window.__env.apiBaseUrl = 'https://76.155.212.102:3000';
 })(this);
