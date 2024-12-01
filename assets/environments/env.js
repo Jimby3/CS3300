9337148bed3e1e4d47100853a4b24fd681ff7b1e
jimby3 <jimever333@gmail.com>
2024-12-01 11:23:16 -0700

    Auto-generated commit

--- assets/environments/env.js
+++ assets/environments/env.js
@@ -2,5 +2,5 @@
   window.__env = window.__env || {};
 
   // Local backend base URL
-  window.__env.apiBaseUrl = 'https://76.155.212.102:80';
+  window.__env.apiBaseUrl = 'https://76.155.212.102:3000';
 })(this);
